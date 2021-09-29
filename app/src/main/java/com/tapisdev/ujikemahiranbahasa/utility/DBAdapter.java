dce79d6f8cd6ec6fc0801eb5ca0ade0ee96c8098
ArifGlory <arifglory46@gmail.com>
2021-09-29 14:44:02 +0700

    pertama dan add library db sqllite

---
+++ app/src/main/java/com/tapisdev/ujikemahiranbahasa/utility/DBAdapter.java
@@ -0,0 +1,215 @@
+package com.tapisdev.ujikemahiranbahasa.utility;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Glory on 28/09/2016.
+ */
+public class DBAdapter extends SQLiteAssetHelper {
+
+    private static final String DB_NAME                 = "db_skbpenyuluh_v3";
+    private static final int        DB_VER              = 1;
+    public static final String TABLE_SOAL               = "tb_soal_paket_1";
+    public static final String TABLE_SOAL_2             = "tb_soal_paket_2";
+    public static final String TABLE_SOAL_3             = "tb_soal_paket_3";
+    public static final String TABLE_SOAL_4             = "tb_soal_paket_4";
+    public static final String TABLE_SOAL_5             = "tb_soal_paket_5";
+    public static final String TABLE_SOAL_6             = "tb_soal_paket_6";
+    public static final String TABLE_SOAL_7             = "tb_soal_paket_7";
+    public static final String TABLE_SOAL_8             = "tb_soal_paket_8";
+    public static final String TABLE_SOAL_9             = "tb_soal_paket_9";
+    public static final String TABLE_SOAL_10             = "tb_soal_paket_10";
+    public static final String TABLE_HISTORY            = "tb_history";
+
+    public static final String COL_SOAL_ID             = "id";
+    public static final String COL_SOAL_SOAL           = "soal";
+    public static final String COL_SOAL_JAWABAN_A      = "jawaban_a";
+    public static final String COL_SOAL_JAWABAN_B      = "jawaban_b";
+    public static final String COL_SOAL_JAWABAN_C      = "jawaban_c";
+    public static final String COL_SOAL_JAWABAN_D      = "jawaban_d";
+    public static final String COL_SOAL_JAWABAN_BENAR  = "jawaban_benar";
+
+    public static final String COL_HISTORI_NAMA_PAKET      = "nama_paket_soal";
+    public static final String COL_HISTORI_SKOR            = "skor";
+    public static final String COL_HISTORI_WAKTU           = "waktu";
+
+    public static final String TAG_INSERT  = "insertDB";
+
+    public static DBAdapter        dbInstance;
+    public static SQLiteDatabase db;
+
+    String[] namaLevel  =   {"Paket Soal 1","Paket Soal 2","Paket Soal 3","Paket Soal 4","Paket Soal 5",
+            "Paket Soal 6","Paket Soal 7","Paket Soal 8","Paket Soal 9","Paket Soal 10"};
+
+
+
+    /**
+     * private Constructor, untuk menggunakan kelas ini gunakan getInstance()
+     * @param context
+     */
+
+
+    private DBAdapter(Context context) {
+        super(context, DB_NAME, null, DB_VER);
+    }
+
+
+    public  static synchronized DBAdapter getInstance(Context context){
+
+        if (dbInstance == null){
+            dbInstance = new DBAdapter(context.getApplicationContext());
+            db = dbInstance.getReadableDatabase();
+        }
+
+        return dbInstance;
+    }
+
+    public SQLiteDatabase ambilDB(){
+        db = this.getWritableDatabase();
+        return db;
+
+
+    }
+
+   /* public DBAdapter getInstance(Context context){
+
+        if (dbInstance == null){
+
+            dbInstance = new DBAdapter(context);
+            db = dbInstance.getReadableDatabase();
+
+        }
+
+        return  dbInstance;
+    }*/
+
+
+
+    @Override
+    public synchronized void close(){
+
+        super.close();
+        if (dbInstance!=null){
+
+            dbInstance.close();
+        }
+    }
+
+
+//method untuk mengambil semua data soal
+    /*public List<Quiz> getAllSoal(String namaPaket){
+
+        List<Quiz> listSoal = new ArrayList<Quiz>();
+        String tabel_soal = "";
+        if (namaPaket.equals(namaLevel[0])){
+            tabel_soal = TABLE_SOAL;
+        }else if (namaPaket.equals(namaLevel[1])){
+            tabel_soal = TABLE_SOAL_2;
+        }else if (namaPaket.equals(namaLevel[2])){
+            tabel_soal = TABLE_SOAL_3;
+        }else if (namaPaket.equals(namaLevel[3])){
+            tabel_soal = TABLE_SOAL_4;
+        }else if (namaPaket.equals(namaLevel[4])){
+            tabel_soal = TABLE_SOAL_5;
+        }else if (namaPaket.equals(namaLevel[5])){
+            tabel_soal = TABLE_SOAL_6;
+        }else if (namaPaket.equals(namaLevel[6])){
+            tabel_soal = TABLE_SOAL_7;
+        }else if (namaPaket.equals(namaLevel[7])){
+            tabel_soal = TABLE_SOAL_8;
+        }else if (namaPaket.equals(namaLevel[8])){
+            tabel_soal = TABLE_SOAL_9;
+        }else if (namaPaket.equals(namaLevel[9])){
+            tabel_soal = TABLE_SOAL_10;
+        }
+
+        Cursor cursor = db.query(tabel_soal,new String[]{
+
+               COL_SOAL_ID,
+                COL_SOAL_SOAL,
+                COL_SOAL_JAWABAN_A,
+                COL_SOAL_JAWABAN_B,
+                COL_SOAL_JAWABAN_C,
+                COL_SOAL_JAWABAN_D,
+                COL_SOAL_JAWABAN_BENAR
+                },null,null,null,null,null);//kenapa ada 5 null ya ?
+
+        if (cursor.moveToFirst()){
+
+            do {
+                Quiz quiz = new Quiz();
+
+                quiz.setId(cursor.getInt(cursor.getColumnIndexOrThrow(COL_SOAL_ID)));
+                quiz.setSoal(cursor.getString(cursor.getColumnIndexOrThrow(COL_SOAL_SOAL)));
+                quiz.setJawaban_a(cursor.getString(cursor.getColumnIndexOrThrow(COL_SOAL_JAWABAN_A)));
+                quiz.setJawaban_b(cursor.getString(cursor.getColumnIndexOrThrow(COL_SOAL_JAWABAN_B)));
+                quiz.setJawaban_c(cursor.getString(cursor.getColumnIndexOrThrow(COL_SOAL_JAWABAN_C)));
+                quiz.setJawaban_d(cursor.getString(cursor.getColumnIndexOrThrow(COL_SOAL_JAWABAN_D)));
+                quiz.setJawaban_benar(cursor.getString(cursor.getColumnIndexOrThrow(COL_SOAL_JAWABAN_BENAR)));
+
+            listSoal.add(quiz);
+            }while (cursor.moveToNext());
+
+        }
+
+        return listSoal;
+    }
+
+    public List<History> getAllHistory(){
+        List<History> listHistory = new ArrayList<>();
+
+        Cursor cursor = db.query(TABLE_HISTORY,new String[]{
+
+                COL_SOAL_ID,
+                COL_HISTORI_NAMA_PAKET,
+                COL_HISTORI_SKOR,
+                COL_HISTORI_WAKTU,
+        },null,null,null,null,null);//kenapa ada 5 null ya ?
+
+        if (cursor.moveToFirst()){
+
+            do {
+                Quiz quiz = new Quiz();
+                History history = new History();
+
+                history.setId(cursor.getInt(cursor.getColumnIndexOrThrow(COL_SOAL_ID)));
+                history.setNamaPaket(cursor.getString(cursor.getColumnIndexOrThrow(COL_HISTORI_NAMA_PAKET)));
+                history.setSkor(cursor.getString(cursor.getColumnIndexOrThrow(COL_HISTORI_SKOR)));
+                history.setWaktu(cursor.getString(cursor.getColumnIndexOrThrow(COL_HISTORI_WAKTU)));
+
+                listHistory.add(history);
+            }while (cursor.moveToNext());
+
+        }
+
+        return listHistory;
+    }*/
+
+    public long insertHistory(String namaPaket, String skor,String waktu) {
+        final SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues initialValues = new ContentValues();
+        long retval = 0;
+        try {
+            initialValues.put("nama_paket_soal", namaPaket);
+            initialValues.put("skor", skor);
+            initialValues.put("waktu", waktu);
+            retval = db.insert(TABLE_HISTORY, null, initialValues);
+        } catch (Exception e) {
+            Log.e(TAG_INSERT, "insertRow exception", e);
+        } finally {
+            db.close();
+        }
+        return retval;
+    }
+
+
+}
