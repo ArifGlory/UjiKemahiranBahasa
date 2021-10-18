65a9b4e5861e1f5f17c80b50fc8fc31707e1d58c
ArifGlory <arifglory46@gmail.com>
2021-10-18 20:26:23 +0700

    fix quiz membaca

--- app/src/main/java/com/tapisdev/ujikemahiranbahasa/activity/LandingListeningActivity.kt
+++ app/src/main/java/com/tapisdev/ujikemahiranbahasa/activity/LandingListeningActivity.kt
@@ -1,13 +1,16 @@
 package com.tapisdev.ujikemahiranbahasa.activity
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Parcelable
 import android.util.Log
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import com.tapisdev.lokamotor.base.BaseActivity
 import com.tapisdev.myapplication.model.SharedVariable
+import com.tapisdev.ujikemahiranbahasa.MainActivity
 import com.tapisdev.ujikemahiranbahasa.R
 import com.tapisdev.ujikemahiranbahasa.model.SeksiMendengarkan
 import java.io.Serializable
@@ -16,6 +19,7 @@ import java.util.*
 class LandingListeningActivity : BaseActivity() {
 
     lateinit var startTesListening : Button
+    var listener: DialogInterface.OnClickListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,12 +36,31 @@ class LandingListeningActivity : BaseActivity() {
             Log.d("randomPaket"," : "+randomPaket)
             SharedVariable.activePaket = randomPaket
 
-            val i = Intent(this,QuizMendengarkanActivity::class.java)
+            val i = Intent(this,QuizMembacaActivity::class.java)
            // i.putExtra("sm",sM as Parcelable)
             i.putExtra("randomPaket",randomPaket)
             startActivity(i)
 
 
         }
+    }
+
+    override fun onBackPressed() {
+        val builder =
+            AlertDialog.Builder(this)
+        builder.setMessage("Apakah anda ingin keluar dari Tes yang sedang berlangsung ?")
+        builder.setCancelable(false)
+
+        listener = DialogInterface.OnClickListener { dialog, which ->
+            if (which == DialogInterface.BUTTON_POSITIVE) {
+                startActivity(Intent(this, MainActivity::class.java))
+            }
+            if (which == DialogInterface.BUTTON_NEGATIVE) {
+                dialog.cancel()
+            }
+        }
+        builder.setPositiveButton("Yes", listener)
+        builder.setNegativeButton("No", listener)
+        builder.show()
     }
 }
